c7520d92eace998f9d700b66b8063d0a92776fcb
jocoonopa <jocoonopa@gmail.com>
2017-07-16 13:50:06 +0800

    8.2.4 Completed
    1. 指標和陣列的關係介紹
    2. 指標的增減量運算
    3. 陣列變數名稱 即 指標變數名稱

--- chp8/array.cpp
+++ chp8/array.cpp
@@ -4,12 +4,29 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int array[10];
+    int array[5] = { 1, 2, 3, 4, 5 };
     int *arrayPtr;
+    int *elementPtr;
     
     arrayPtr = array;
 
+    elementPtr = &array[3];
 
+    cout << arrayPtr[3] << endl;
+    cout << *elementPtr << endl;
+
+
+    cout << *arrayPtr << endl;
+    cout << *(arrayPtr + 1) << endl;
+    cout << *(arrayPtr + 2) << endl;
+    cout << *(arrayPtr + 3) << endl;
+    cout << *(arrayPtr + 4) << endl;
+
+    cout << *(++ arrayPtr) << endl;
+    cout << *(++ arrayPtr) << endl;
+    cout << *(++ arrayPtr) << endl;
+    cout << *(++ arrayPtr) << endl;
+    
 
     return 0;
 }
